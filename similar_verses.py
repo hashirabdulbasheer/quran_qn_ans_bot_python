682c641fd8ad1cc2f84fa921f705f84e2072f594
Hashir Abdulbasheer <hashir.abdulbasheer@gmail.com>
2023-07-10 15:00:11 +0530

    new: added a class to find similar verses by comparing the dot product of question with the rest of the verses

---
+++ similar_verses.py
@@ -0,0 +1,54 @@
+import numpy as np
+import json
+import itertools
+from langchain.embeddings.openai import OpenAIEmbeddings
+
+class QuranSimilarVerses:
+    quran_data = None
+    translation = None
+    embedding = OpenAIEmbeddings()
+
+    def __init__(self, translation):
+        self.quran_data = np.load("out_wahiddudin.npy", allow_pickle=True)
+        trans_file = open(translation)
+        trans_json = json.load(trans_file)
+        trans_file.close()
+        self.translation = trans_json
+
+
+    def get_verse(self, surah, aya):
+        return [d for d in self.translation["quran"] if int(d['chapter']) == int(surah) and int(d['verse']) == int(aya)][0]["text"]
+
+
+    def get_similar(self, question):  
+        if self.quran_data is None:
+            return "Error: Loading data, please try again after some time"      
+        
+        embedding_input_string = self.embedding.embed_query(question)
+        result = {}
+        for key in self.quran_data.item():
+            result[key] = np.dot(embedding_input_string, self.quran_data.item()[key])
+
+        # sorting in descending order
+        sorted_result = dict(sorted(result.items(), key=lambda item: item[1], reverse=True))       
+
+        response = "Qn: " +  question
+        response = response + "\nVerses:\n"
+
+        # get top N results
+        N = 4
+        sliced_result = dict(itertools.islice(sorted_result.items(), N))
+
+        for item in sliced_result:
+            indices = item.split("_")
+            surah = indices[0]
+            aya = indices[1]
+            response = response + str(surah) + ":" + str(aya) + " " + self.get_verse(surah, aya) + "\n"
+            response = response + "https://uxquran.com/apps/quran-ayat/?sura=" + surah + "&aya=" + aya + "\n\n"
+
+        return response            
+    
+
+quran = QuranSimilarVerses("input.json")
+answer = quran.get_similar("How many years did Ashabul Khaf sleep in the cave")    
+print(answer)
