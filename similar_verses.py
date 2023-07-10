7ccd41f830155f1b8721d1361c7b6051a8387706
Hashir Abdulbasheer <hashir.abdulbasheer@gmail.com>
2023-07-10 15:23:51 +0530

    config: updated to pass the quran embeddings as a paramter to the QuranSimilarVerses class

--- similar_verses.py
+++ similar_verses.py
@@ -1,3 +1,11 @@
+#
+# This chatbot is designed to provide relevant verses from the Noble Quran
+# that may address a question, along with a potential answer derived
+# from those verses using an AI-powered language model
+#
+# uxquran.com
+#
+
 import numpy as np
 import json
 import itertools
@@ -8,8 +16,8 @@ class QuranSimilarVerses:
     translation = None
     embedding = OpenAIEmbeddings()
 
-    def __init__(self, translation):
-        self.quran_data = np.load("out_wahiddudin.npy", allow_pickle=True)
+    def __init__(self, quran_embeddings, translation):
+        self.quran_data = np.load(quran_embeddings, allow_pickle=True)
         trans_file = open(translation)
         trans_json = json.load(trans_file)
         trans_file.close()
@@ -20,17 +28,17 @@ class QuranSimilarVerses:
         return [d for d in self.translation["quran"] if int(d['chapter']) == int(surah) and int(d['verse']) == int(aya)][0]["text"]
 
 
-    def get_similar(self, question):  
+    def get_similar(self, question):
         if self.quran_data is None:
-            return "Error: Loading data, please try again after some time"      
-        
+            return "Error: Loading data, please try again after some time"
+
         embedding_input_string = self.embedding.embed_query(question)
         result = {}
         for key in self.quran_data.item():
             result[key] = np.dot(embedding_input_string, self.quran_data.item()[key])
 
         # sorting in descending order
-        sorted_result = dict(sorted(result.items(), key=lambda item: item[1], reverse=True))       
+        sorted_result = dict(sorted(result.items(), key=lambda item: item[1], reverse=True))
 
         response = "Qn: " +  question
         response = response + "\nVerses:\n"
@@ -46,9 +54,9 @@ class QuranSimilarVerses:
             response = response + str(surah) + ":" + str(aya) + " " + self.get_verse(surah, aya) + "\n"
             response = response + "https://uxquran.com/apps/quran-ayat/?sura=" + surah + "&aya=" + aya + "\n\n"
 
-        return response            
-    
+        return response
 
-quran = QuranSimilarVerses("input.json")
-answer = quran.get_similar("How many years did Ashabul Khaf sleep in the cave")    
+
+quran = QuranSimilarVerses("out_wahiddudin.npy", "input.json")
+answer = quran.get_similar("How many years did Ashabul Khaf sleep in the cave")
 print(answer)
